8b845ff7dfce038b80dd1251544884c451690c51
Alessandro <alessandropadilharocha@gmail.com>
2025-08-18 18:36:35 +0000

    estudos 18-08

---
+++ manipulacao_variaveis.c
@@ -0,0 +1,59 @@
+/*
+#include <stdio.h>
+ 
+int main() {
+    int a = 10;
+    int b = 3;
+    int soma = a + b;
+    int diferenca = a - b;
+    int produto = a * b;
+    int quociente = a / b; // Note que a divisão de inteiros resulta em um número inteiro
+ 
+    printf("Soma: %d\n", soma);
+    printf("Diferença: %d\n", diferenca);
+    printf("Produto: %d\n", produto);
+    printf("Quociente: %d\n", quociente);
+ 
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+ 
+int main() {
+    int a = 10;
+    int b = 3;
+    float quociente = (float) a / b; // 'a' é explicitamente convertido para float
+ 
+    printf("Quociente: %.2f\n", quociente);
+ 
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+int main () {
+
+    int a = 10;
+    float b = 3.5;
+    float resultado = a + b;
+
+    printf("Resultado: %.2f\n", resultado);
+
+    return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main () {
+
+    int a = 10;
+    int b = 3;
+    float quociente = (float) a / b;
+
+    printf("Quociente: %.2f\n", quociente);
+
+    return 0;
+}
