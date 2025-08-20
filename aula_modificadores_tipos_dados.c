b3404bd0f89deca589ed08ea28b6981721fa696e
Alessandro <alessandropadilharocha@gmail.com>
2025-08-20 21:27:07 +0000

    aula 20-08

---
+++ aula_modificadores_tipos_dados.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main () {
+/*
+    int signedNumber = 3000000000;
+    unsigned int unsignedNumber = 3000000000;
+
+    printf ("Número com sinal: %d\n", signedNumber);
+    printf ("Número sem sinal: %u\n", unsignedNumber);
+
+    int regularNumber = 2147483647;
+    long int bigNumber = 2147483647;
+
+    printf ("Número regular (int): %d\n", regularNumber);
+    printf ("Número grande (long int): %ld\n", bigNumber);
+
+    bigNumber = 2147483648;
+
+    printf("Número grande atualizado (long int): %ld\n", bigNumber);
+
+
+    double preciseNumber = 3.141592653589793;
+    long double veryPreciseNumber = 3.14159265358979323846;
+
+    printf ("Número preciso (double): %.15f\n", preciseNumber);
+    printf("Número muito preciso (long double): %.21Lf\n", veryPreciseNumber);
+
+    unsigned long int largePositiveNumber = 4000000000;
+    printf("Número positivo grande: %lu\n", largePositiveNumber);
+*/
+    
+
+    return 0;
+}
