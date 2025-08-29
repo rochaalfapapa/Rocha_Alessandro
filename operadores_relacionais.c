8dbb70b4e25a1be140defde8f9b31578a583485e
Alessandro <alessandropadilharocha@gmail.com>
2025-08-29 21:02:27 +0000

    Estudos 29-08-25

---
+++ operadores_relacionais.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+
+    /*
+    int x = 5;
+    float y = 5.0;
+    char c = 'a';
+
+    printf("x >= y: %d\n", x >= y);
+    printf("x == y: %d\n", x == y);
+    printf("x != y: %d\n", x != y);
+
+    printf("x >= c: %d\n", x >= c);
+    printf("O valor ASCII de %c é %d: \n", c, c);
+
+    */
+
+    float numero1 = 10.2;
+    int numero2 = 10;
+
+    printf ("numero1 > numero2: %d\n", (int) numero1 > numero2);
+    printf ("numero1 == numero2: %d\n", (int) numero1 == numero2);
+    
+    return 0;
+}
