d8007d6dcc2d4e710206395e5291ca63c8c398ea
Alessandro <alessandropadilharocha@gmail.com>
2025-11-13 22:17:30 +0000

    estudos_2025_11_13

---
+++ aula_Estrutura_Decisao.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+int main (){
+    /*
+    int numero1, numero2;
+
+    numero1 = 10;
+    numero2 = 15;
+
+    if (numero1 > numero2){
+        printf("Número 1 é maior do que número 2\n");
+
+    }
+    
+    printf("Fora IF\n");
+    */
+
+    /*
+    int idade = 2;
+
+    if (idade >= 18) {
+
+        printf("Você é maior de idade\n");
+    }
+    
+    printf("Fora IF\n");
+    */
+
+    /*
+    float temp = 30.0;
+
+    if (temp >= 30.0) {
+
+        printf("Está calor\n");
+
+    }
+
+    printf("Fora IF\n");
+    */
+
+    /*
+    int nota = 60;
+
+    if(nota >= 60) {
+
+        printf("Parabéns, você passou!\n");
+    }
+
+    printf("Fora IF\n");
+    */
+
+    /*
+    int idade1, idade2;
+    idade1 = 35;
+    idade2 = 30;
+
+    if(idade1 > idade2){
+
+        printf("Pessoa 1 é mais velha que a Pessoa 2\n");
+    }
+    printf("Fora IF\n");
+    */
+
+    /*
+    int number = 4;
+    int result;
+
+    result = number % 2;
+
+    printf("A variável resultado é: %d\n", result);
+
+    if (result == 0){
+
+        printf("O número é par\n");
+    }
+
+    printf("Fora IF\n");
+    */
+
+    int estoque = 3;
+
+    if (estoque < 5) {
+
+        printf("Estoque baixo\n");
+    }
+
+    printf("Fora IF\n");
+
+    return 0;
+}
