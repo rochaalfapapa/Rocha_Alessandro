8cbd47ddd93b5ad27594611b2236d5136b680b47
Alessandro <alessandropadilharocha@gmail.com>
2025-12-10 18:44:23 +0000

    study_2025_12_10

---
+++ cuso_logica_programacao_Python/a.py
@@ -0,0 +1,10 @@
+num = 50.0
+print(num)
+print(type(num))
+name = "Alessandro"
+print(name)
+print(type(name))
+print(2<3)
+test = "Este é um teste de manipulação de strings"
+print(test)
+print(test.split())
